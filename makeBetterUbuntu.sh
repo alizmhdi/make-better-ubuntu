cbb83bf2ffce1b82739d6f2fbc4007f268029c55
alizademhdi <alizademhdi@gmail.com>
2022-03-22 17:50:58 +0430

    push script

---
+++ makeBetterUbuntu.sh
@@ -0,0 +1,63 @@
+#!bin/bash
+
+#something
+sudo apt update
+sudo apt install curl
+sudo apt install vim
+sudo apt install git
+sudo apt install terminator
+sudo snap install postman
+
+
+#python
+sudo apt install -y python3 python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+python --version
+pip --version
+#pytcharm
+sudo snap install pycharm-community --classic
+#python package
+pip install requests
+
+
+#docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+
+
+#social
+sudo snap install telegram-desktop
+sudo snap install discord
+
+wget https://go.skype.com/skypeforlinux-64.deb
+sudo apt install ./skypeforlinux-64.deb
+
+
+#media
+sudo apt install vlc
+
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client
+
+
+#vscode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+
+#brave
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+
